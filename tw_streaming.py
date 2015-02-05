711416b0329b12f7e7f7640ea4cdc3b9104824c8
joausaga <jorgesaldivar@gmail.com>
2015-02-05 18:04:53 +0100

    Add statements to clean the text of the tweets

--- tw_streaming.py
+++ tw_streaming.py
@@ -4,25 +4,25 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 import codecs
+import ConfigParser
 import sys
 import signal
+import os.path
 
-# Go to http://dev.twitter.com and create an app.
-# The consumer key and secret will be generated for you after
-consumer_key="PVBkth1IJfit6XhVSdxXQwa3u"
-consumer_secret="CrA8TVEnr3IUJ3idHerTOkY9ApZRfKmOjRzyfG7OghvMEOMmFY"
 
-# After the step above, you will be redirected to your app's page.
-# Create an access token under the the "Your access token" section
-access_token="2733258272-akrGnoZLYKJ5srQNFORgYHNfnOo5JivVZBjI8Bv"
-access_token_secret="yyNdrOOOWC7eXOsA8gsI6Tke0ZQnHIX0aGzdVWzucGZpK"
+config = ConfigParser.ConfigParser()
+config.read('config')
+
+consumer_key = config.get('apikey', 'key')
+consumer_secret = config.get('apikey', 'secret')
+access_token = config.get('token', 'token')
+access_token_secret = config.get('token', 'secret')
 
 class StdOutListener(StreamListener):
     
     def __init__(self, f):
         super(StdOutListener, self).__init__()
         self.f = f
-        self.f.write("\"date\",\"id\",\"keyword\",\"user_handler\",\"text\",\"url\"\n")
         self.url = "http://www.twitter.com"
         self.keywords = [{"id":"obamacare","terms":["obamacare"]},
                          {"id":"college","terms":["tuition price cost", "colleges universities college university"]},
@@ -32,7 +32,6 @@ class StdOutListener(StreamListener):
                          {"id":"gay","terms":["marriage", "rights law laws", "gay same-sex partners"]}]
     
     def on_status(self, status):
-	#print("@%s: %s" % (status.author.screen_name, status.text.encode("ascii","ignore")))
         for keyword in self.keywords:
             arr_terms = keyword.get("terms")
             found_terms = True
@@ -40,8 +39,9 @@ class StdOutListener(StreamListener):
                 if not any(term in status.text.lower().split() for term in terms.split()):
                     found_terms = False
             if found_terms:
-                #print("@%s: %s" % (status.author.screen_name, status.text.encode("ascii","ignore")))
-                #print("Found a tweet from California containg the keyword %s!" % keyword.get("id"))
+                tw_text = status.text.encode("ascii","ignore")
+                tw_text = tw_text.replace('\n', ' ').replace('\r', '')
+                tw_text = tw_text.replace('\"', '\'')
                 url_status = self.url + "/" + status.author.screen_name + "/status/" + status.id_str
                 self.f.write("\"%s\",\"%s\",\"%s\",\"@%s\",\"%s\",\"%s\"\n" % (status.created_at,status.id_str,keyword.get("id"),
                                                                                status.author.screen_name,
@@ -56,14 +56,18 @@ class StdOutListener(StreamListener):
 
 if __name__ == '__main__':
     try:
+        fname = "output.csv"
+        file_exists = False
+        if os.path.isfile(fname):
+            file_exists = True
         f_output = codecs.open("output.csv", "a", encoding="ascii")
+        if !file_exists:
+            f_output.write("\"date\",\"id\",\"keyword\",\"user_handler\",\"text\",\"url\"\n")
         l = StdOutListener(f_output)
         auth = OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_token, access_token_secret)
         stream = Stream(auth, l)
         stream.filter(locations=[-124.24,32.30,-120,42])  # Only tweets from California
-        #stream.filter(track=['obamacare'], locations=[-124.24,32.30,-120,42])
-        #stream.filter(track=['obamacare', 'k-12 public education', 'immigrant service access', 'marijuana recreation law', 'gay marriage rights'])
     except KeyboardInterrupt:
         f_output.close()
 
